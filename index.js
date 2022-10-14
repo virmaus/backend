1d279e6a1f9f5199b989a67b3f283b0af069d0a6
Your Name <you@example.com>
2022-10-14 15:23:36 -0300

    conexion a bd

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ var mongoose= require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3700;
 
+const usuario = 'madmint';
+const password = 'M4dm1nt25';
+const dbName = "portafolio"
+
+const uri = `mongodb+srv://${usuario}:${password}@cluster0.dbhrgys.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/portafolio')
+mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true })
         .then(() =>{
             console.log("conexion a la base de datos establecida con exito...");
 
